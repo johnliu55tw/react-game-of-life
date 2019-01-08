72bb3b62385959b4e0dde058d56e024f075115f4
John Liu <johnliu55tw@gmail.com>
2019-01-08 22:15:17 +0800

    Added index.css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { GamePresenter } from './game_of_life/presenter.js'
+import './index.css'
 import * as serviceWorker from './serviceWorker'
 
+function GithubRibbon (props) {
+  const style = {position: 'absolute', top: 0, right: 0, border: 0}
+  return (
+    <a href='https://github.com/johnliu55tw/react-game-of-life'>
+      <img
+        style={style}
+        src='https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png'
+        alt='Fork me on GitHub' />
+    </a>
+  )
+}
+
 ReactDOM.render(
-  <div className='body'>
-    <GamePresenter
-      width={81}
-      height={61}
-      maxSpeed={20}
-    />
+  <div className='container'>
+    <GithubRibbon />
+    <div className='body'>
+      <h1>Game of Life in React.js</h1>
+      <GamePresenter
+        width={81}
+        height={61}
+        maxSpeed={20}
+      />
+    </div>
   </div>,
   document.getElementById('root')
 )
